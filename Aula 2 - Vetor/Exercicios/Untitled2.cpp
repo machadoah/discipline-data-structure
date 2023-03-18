23c3a7ad3c09db34621658043d0dd5e701fb10f1
Antonio Henrique Machado <antoniohenriquemachadoadm@gmail.com>
2023-03-18 17:19:38 -0300

    Create Untitled2.cpp

---
+++ Aula 2 - Vetor/Exercicios/Untitled2.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib> 
+#include <iostream>
+#include <math.h> // uso das funções matemáticas
+
+// include representa os imports
+
+using namespace std; // cout e cin
+
+int main() {
+	
+	// Define a localização para Português-Brasil
+    setlocale(LC_CTYPE, "portuguese_brazil");
+    
+    /*
+	Criar um programa que leia dados de um vetor de 12 elementos inteiros.
+	Imprima o maior e menor sem ordenar, o percentual de números pares e a média dos elementos do vetor. 
+	*/
+	
+
+	return 0;
+}
