f8b458791a43e7fdb5e26f34e1d979609bae9ed8
machadoah <antoniohenriquemachadoadm@gmail.com>
2023-03-21 22:02:03 -0300

    Create Sem Título1.cpp

---
+++ Trabalho semestral/Sem Título1.cpp
@@ -0,0 +1,59 @@
+#include <cstdlib> 
+#include <iostream>
+#include <math.h> // uso das funções matemáticas
+
+using namespace std;
+
+int fibonacci(int n){
+	
+	if((n == 1) || (n == 2)) {
+		return 1;
+		
+	} else {
+		
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
+
+int main()
+{
+	// Heap Sort
+	
+	/*
+	
+	- (1) Introdução sobre a origem do algoritmo
+	
+		-
+	
+	- (2) O processo de funcionamento
+	
+		- 
+	
+	- (3) Melhor cenário e pior cenário
+	
+		-
+	
+	- (4) Aplicabilidade
+	
+		- 
+	
+	- (5) Código na linguagem que desejar
+	
+		- 
+		-
+	
+	- (6) Conclusão
+	
+		- 
+		
+	- (7) Parte escrita e slide
+	
+		- 
+	
+	*/
+	
+
+    return 0;
+}
+
+
